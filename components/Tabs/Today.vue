2319e60fdcb73d2270f9ed3222ea3f608f6f9636
Tobi-Daniel <oloyebmv@gmail.com>
2024-05-29 21:24:13 +0100

    create dashboard tabs

---
+++ components/Tabs/Today.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+Hello today
+    </div>
+</template>
+
+<script setup>
+const loading = ref(false);
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
-<script setup>
+<script setup lang="ts">
 const loading = ref(false);
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const list = [
+  {
+    title: "Today",
+    component: resolveComponent("TabsToday"), // custom component relative to today
+  },
+  {
+    title: "Week",
+    component: "<div>today</div>",
+  },
+  {
+    title: "Month",
+    component: "<div>today</div>",
+  },
+  {
+    title: "Year",
+    component: "<div>today</div>",
+  },
+];
 </script>
 
 <template>
@@ -13,16 +33,38 @@ const loading = ref(false);
     </header>
 
     <main class="grid gap-4">
-      <div class="flex items-center gap-4">
+      <!-- <div class="flex items-center gap-4">
         <div  v-for="(item, index) in 3" :key="index" class="w-[120px] h-[36px] bg-neutral-200"></div>
       </div>
       <section>
         <div class="w-full h-[360px] bg-neutral-200"></div>
-      </section>
+      </section> -->
+      <Tabs default-value="today" class="w-[400px]">
+        <TabsList>
+          <TabsTrigger
+            v-for="(item, index) in list"
+            :key="index"
+            :value="item.title"
+          >
+            {{ item.title }}
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent
+          v-for="(item, index) in list"
+          :key="index"
+          :value="item.title"
+        >
+          {{ item.component }}
+        </TabsContent>
+      </Tabs>
     </main>
     <footer>
       <div class="flex items-center gap-4">
-        <div v-for="(item, index) in 3" :key="index" class="w-full h-[260px] bg-neutral-200"></div>
+        <div
+          v-for="(item, index) in 3"
+          :key="index"
+          class="w-full h-[260px] bg-neutral-200"
+        ></div>
       </div>
     </footer>
   </div>
